3a1d560b4af3108a7c44bfb1dfad69d41a23441e
RidaNaz <rnaz3414@gmail.com>
2024-02-24 08:34:05 +0500

    commit by Rida

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
-        {/* <Education /> */}
         <Certificate/>
         <Projects />
       </div>
